12a19cd8e7f90d4eb4c9d116c0971553f1f83dbb
ssham <sergshamiryan@gmail.com>
2023-12-09 22:37:11 +0400

    Structural: Adapter Problem 1

--- src/main/java/org/example/structural/adapter/problem_1/VehicleKMHAdapter.java
+++ src/main/java/org/example/structural/adapter/problem_1/VehicleKMHAdapter.java
@@ -1,11 +1,16 @@
 package org.example.structural.adapter.problem_1;
 
+import java.util.concurrent.Flow;
+
 public abstract class VehicleKMHAdapter extends Vehicle
         implements VehicleAdapterService {
 
     public VehicleKMHAdapter(String brand) {
         super(brand);
     }
+
+
+
 
     public int getSpeed() {
         return speedKMH();
